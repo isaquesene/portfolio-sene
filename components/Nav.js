f56d0e256ee138746581de4bab2649ec67065ab4
isaque sene <isaqueconcursos26@gmail.com>
2023-07-10 18:20:43 -0300

    feat: navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,14 @@
+import{
+  HiHome,
+  HiUser,
+  HiViewColumns,
+  HiRectangleGroup,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope
+} from 'react-icons/hi2';
+
 //  links
-const links = [
+export const navData = [
   { name: 'home', path: '/', icon: <HiHome /> },
   { name: 'about', path: '/about', icon: <HiUser /> },
   { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
@@ -16,8 +25,42 @@ const links = [
   },
 ];
 
+//next link
+import Link from "next/link";
+
+//next router
+import {useRouter} from 'next/router';
+import { userAgent } from 'next/server';
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+  return (
+    <nav className='flex flex-col inter-center 
+    xl:justify-center gap-y-4 
+    fixed h-max bottom-0 mt-auto xl: right-[2%] 
+    z-50 top-0 w-full xl:w-16 xl:max-w-md
+    bg-red-500/30 p-20'>
+      {/* inner */}
+      <div className='
+      flex w-full 
+      xl:flex-col 
+      items-center 
+      justify-between 
+      xl:justify-center 
+      gap-y-10 px-4 md:px-40 
+      xl:px-0 h-80
+      xl:h-max
+      py-8 bg-white/10
+      backdrop-blur-sm 
+      text-3xl xl:text-xl
+      xl:rounded-full'>
+        {navData.map((link, index) => {
+          return <Link href={link.path}>{link.icon}</Link>;
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
