bbab83edd68818700959401a71faa3777651e241
isaque sene <isaqueconcursos26@gmail.com>
2023-07-10 19:09:06 -0300

    feat: navbar right

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 const TopLeftImg = () => {
   return (
     <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
-      <Image src='/top-left-img.png' width={400} height={400} alt=""/>
+      <Image src='/_LOGO_2.png' width={400} height={400} alt=""/>
     </div>
   ); 
 };
