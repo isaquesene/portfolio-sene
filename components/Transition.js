1b708537de110be845006456c1b12e4974259363
isaque sene <isaqueconcursos26@gmail.com>
2023-07-10 19:41:05 -0300

    feat: animate transition 2

--- components/Transition.js
+++ components/Transition.js
@@ -25,9 +25,23 @@ const Transition = () => {
       animate='animate'
       exit='exit'
       transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}
-      >1</motion.div>
-      <div>2</div>
-      <div>3</div>
+      ></motion.div>
+
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]'
+      variants={transitionVariants}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+      transition={{delay: 0.4, duration: 0.6, ease: 'easeInOut'}}
+      ></motion.div>
+
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]'
+      variants={transitionVariants}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+      transition={{delay: 0.6, duration: 0.6, ease: 'easeInOut'}}
+      ></motion.div>
     </>
   );
 };
