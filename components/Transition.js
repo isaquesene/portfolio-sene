8dced1ba7b9a3c639c63c22958f85972201b9b8c
isaque sene <isaqueconcursos26@gmail.com>
2023-07-10 19:35:37 -0300

    feat: transition animate

--- components/Transition.js
+++ components/Transition.js
@@ -1,7 +1,33 @@
+//framer motion
+import {motion} from 'framer-motion';
+
+const transitionVariants = {
+  initial: {
+    x: '100%',
+    with: '100%'
+  },
+  animate: {
+    x: '0%',
+    with: '0%'
+  },
+  exit: {
+    x: ['0%', '100%'],
+    with: ['0%', '100%']
+  },
+};
+
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]'
+      variants={transitionVariants}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+      transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}
+      >1</motion.div>
+      <div>2</div>
+      <div>3</div>
     </>
   );
 };
