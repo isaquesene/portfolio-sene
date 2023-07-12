c6a5cbb0414ca4f675bc3dd39f41db708b3cbc98
isaque sene <isaqueconcursos26@gmail.com>
2023-07-12 18:51:11 -0300

    feat: avatar analisar se vou usar ou não

--- pages/index.js
+++ pages/index.js
@@ -52,8 +52,13 @@ const Home = () => {
       absolute mix-blend-color-dodge translate-z-0">
       </div>
       {/* particles */}
-      <div>particles</div>
+      <ParticlesContainer/>
       {/* avatar */}
+      {/* <motion.div
+      variants={fadeIn('up', 0.5)} initial="hidden" animate="show" exit="hidden" transition={{duration: 1, ease: 'easeInOut'}}
+       className="w-full h-full max-w-[737px] max-h-[678px] absolute bottom-32 lg:bottom-0 lg:right-[8%]">
+        <Avatar/>
+      </motion.div> */}
     </div>
   </div>
   );
