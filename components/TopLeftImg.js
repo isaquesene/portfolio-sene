a0d215dcc2464790f9ccb324e130091272434736
isaque sene <isaqueconcursos26@gmail.com>
2023-07-14 18:34:41 -0300

    feat: about skills

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const TopLeftImg = () => {
   return (
-    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
+    <div className="absolute left-0 top-0 mix-blend-color-dodge z-1 w-[200px] xl:w-[400px] opacity-50">
       <Image src='/_LOGO_2.png' width={400} height={400} alt=""/>
     </div>
   ); 
