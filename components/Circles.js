712970bfbb2c88a8d11f2f0feee40a5381ba9830
isaque sene <isaqueconcursos26@gmail.com>
2023-07-14 09:54:00 -0300

    feat: lg

--- components/Circles.js
+++ components/Circles.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Circles = () => {
   return (
-  <div className="w-[200px] xl:w-[350px] absolute -right-5 bottom-1 mix-blend-color-dodge animate-pulse duration-75 z-10">
+  <div className="w-[200px] xl:w-[350px] lg:w-[350px] absolute -right-5 bottom-1 mix-blend-color-dodge animate-pulse duration-75 z-10">
     <Image src={'/cubese.png'} width={260} height={200} className="w-full h-full" alt=""/>
   </div>
   );
