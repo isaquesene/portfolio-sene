41cd19c06a312edd0fb27eebbb72a49cf8d92954
isaque sene <isaqueconcursos26@gmail.com>
2023-07-17 19:34:57 -0300

    feat: services

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,34 +1,89 @@
+//import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+// icons
+import {
+  RxCrop,
+  RxDesktop,
+  RxPencil2,
+  RxReader,
+  RxRocket,
+  RxArrowTopRight,
+} from 'react-icons/rx';
+
+//import required modules
+import { FreeMode, Pagination } from 'swiper';
+ 
 // data
-const serviceData = [
+export const serviceData = [
   {
-    /* icon: <RxCrop />, */
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    icon: <RxCrop />,
+    title: 'DevOps',
+    description: 'Reduce risks and improve the quality of the final product.',
   },
   {
-    /* icon: <RxPencil2 />, */
+    icon: <RxPencil2 />,
     title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Interactive design, aiming at product quality.',
   },
   {
-    /* icon: <RxDesktop />, */
+    icon: <RxDesktop />,
     title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Software & Web Development. current technologies.',
   },
   {
-    /* icon: <RxReader />, */
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    /* icon: <RxRocket />, */
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    icon: <RxRocket />,
+    title: 'Infrastructure',
+    description: 'Deploy support, experience Azure and Linux servers.',
   },
 ];
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return <Swiper breakpoints={{
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 15
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 15
+    },
+  }}
+  freeMode={true}
+  pagination={{
+    clickable: true
+  }}
+  modules={[FreeMode, Pagination]}
+  className='h-[240px] sm:h-[340px]'
+  >
+    {
+      serviceData.map((item, index) => {
+        return(
+          <SwiperSlide key={index}>
+            <div className='bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer
+            hover:bg-rgba(89,169,0.15)] trnasition-all duration-300'>
+              {/* icon */}
+              <div className='text-3xl text-cubes mb-4'>{item.icon}</div>
+              {/* title & desc */}
+              <div className='mb-8'>
+                <div className='mb-2 text-log'>{item.title}</div>
+                <p className='max-w-[350px] leading-normal'>{item.description}</p>
+              </div>
+              {/* arrow */}
+              <div className='text-3xl'>
+                <RxArrowTopRight className='group-hover:rotate-45 group-hover:text-cubes transition-all duration-300'/>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })
+    }
+  </Swiper>;
 };
 
 export default ServiceSlider;
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,9 +1,3 @@
-// icons
-import { RxSlider } from 'react-icons/rx';
-
-//service data
-export const serviceData = [];
-
 //components
 import ServiceSlider from '../../components/ServiceSlider';
 
@@ -23,11 +17,20 @@ const Services = () => {
       <div className='container mx-auto'>
         <div className='flex flex-col xl:flex-row gap-x-8'>
           {/* text */}
-          <div>
-            <h2 className='h2 xl:mt-8'>My services <span className='text-cubes'>.</span></h2>
+          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4'>
+            <motion.h2 variants={fadeIn('up', 0.2)} initial="hidden" animate="show" exit="hidden"
+            className='h2 xl:mt-8'>My services <span className='text-cubes'>.</span></motion.h2>
+            <motion.p variants={fadeIn('up', 0.4)} initial="hidden" animate="show" exit="hidden"
+            className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+            I m a software and web developer, focus on react js, next.js, Php, Ajax, Angular. 
+            I create technological solutions for companies and startups.
+            </motion.p>
           </div>
           {/* slider */}
+          <motion.div variants={fadeIn('up', 0.3)} initial="hidden" animate="show" exit="hidden"
+          className='w-full xl:max-w-[65%]'>
           <ServiceSlider/>
+          </motion.div>
         </div>
       </div>
       <Bulb/>
