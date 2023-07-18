897b957e67e284f1a04148ed8acf708ceac89e02
isaque sene <isaqueconcursos26@gmail.com>
2023-07-18 12:59:25 -0300

    feat: imagens clicadoc & worslider

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,15 +1,15 @@
 // data
-const workSlides = {
+export const workSlider = {
   slides: [
     {
       images: [
         {
           title: 'title',
-          path: '/thumb1.jpg',
+          path: '/landing_page_clicadoc.png',
         },
         {
           title: 'title',
-          path: '/thumb2.jpg',
+          path: '/clicadoc_sistem.png',
         },
         {
           title: 'title',
@@ -44,8 +44,54 @@ const workSlides = {
   ],
 };
 
+//import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+//import required modules
+import { Pagination } from 'swiper';
+
+// icons
+import { BsArrowRight } from 'react-icons/bs';
+
+//image
+import Image from 'next/image';
+ 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return <Swiper 
+  spaceBetween={10}
+  pagination={{
+    clickable: true
+  }}
+  modules={[Pagination]}
+  className='h-[280px] sm:h-[480px]'
+  >
+    {
+      workSlider.slides.map((slide, index) => {
+        return(
+          <SwiperSlide key={index}>
+            <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
+              {slide.images.map((image, index) => {
+                return (
+                  <div key={index} className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
+                    <div className='flex items-center justify-center relative overflow-hidden'>
+                      {/* image */}
+                      <Image src={image.path} width={500} height={300} alt=''/>
+                      {/* overlay gradient */}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </SwiperSlide>
+        );
+      })
+    }
+  </Swiper>;
 };
 
 export default WorkSlider;
