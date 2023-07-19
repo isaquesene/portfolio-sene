e123cf2ce7e6625ea0e349f149b238268ea8e378
isaque sene <isaqueconcursos26@gmail.com>
2023-07-19 12:49:20 -0300

    feat: parcial page: work

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -78,22 +78,25 @@ const WorkSlider = () => {
               {slide.images.map((image, index) => {
                 return (
                   <div key={index} className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
-                    <div className='flex items-center justify-center relative overflow-hidden'>
+                    <div className='flex items-center justify-center relative overflow-hidden group' key={index}>
                       {/* image */}
                       <Image src={image.path} width={500} height={300} alt=''/>
                       {/* overlay gradient */}
-                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#2cffff] to-[#090522] 
+                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#090522] 
                       opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
                       {/* title */}
                       <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20
                       transition-all duration-300'>
                         <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
                           {/* title part 1 */}
-                          <div>title part 1</div>
+                          <div className='delay-100'>LIVE</div>
                           {/* title part 2 */}
-                          <div>title part 2</div>
+                          <div className='translate-y-[500%] group-hover:translate-y-0
+                          transiiton-all duration-300 delay-150'>PROJECT</div>
                           {/* icon */}
-                          <div>icon</div>
+                          <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
+                            <BsArrowRight/>
+                          </div>
                         </div>
                       </div>
                     </div>
--- pages/work/index.js
+++ pages/work/index.js
@@ -19,7 +19,7 @@ const Work = () => {
           {/* text */}
           <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4'>
             <motion.h2 variants={fadeIn('up', 0.2)} initial="hidden" animate="show" exit="hidden"
-            className='h2 xl:mt-8'>My Work <span className='text-cubes'>.</span></motion.h2>
+            className='h2 xl:mt-12'>My Work <span className='text-cubes'>.</span></motion.h2>
             <motion.p variants={fadeIn('up', 0.4)} initial="hidden" animate="show" exit="hidden"
             className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
             I m a software and web developer, focus on react js, next.js, Php, Ajax, Angular. 
